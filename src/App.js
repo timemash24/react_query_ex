4ed4c1fc19f34f0699fd990b32e1c0b4a7b86e68
Timemash <timemash24@gmail.com>
2022-11-23 21:25:54 +0900

    Load data from API using react-query and axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useQuery } from 'react-query';
+import { useEffect } from 'react';
+
+const BASE_URL = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
 function App() {
+  const callApi = async () => {
+    const { data } = await axios.get(BASE_URL);
+    return data;
+  };
+
+  // const getRandRecipe = () => {
+  //   const query = useQuery('recipe', callApi);
+  //   console.log(query);
+  // };
+
+  // useEffect(() => {
+  //   getRandRecipe();
+  // }, []);
+
+  const query = useQuery('recipe', callApi, {
+    refetchOnWindowFocus: false,
+  });
+  console.log(query.data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>react-query example</h1>
     </div>
   );
 }
