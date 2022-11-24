6447625136bdb72730e7c3384ae6353a347b32c1
Timemash <timemash24@gmail.com>
2022-11-24 22:11:55 +0900

    useQuery Example

--- src/App.js
+++ src/App.js
@@ -8,28 +8,39 @@ import { useEffect } from 'react';
 const BASE_URL = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
 function App() {
-  const callApi = async () => {
+  const getRandMeal = async () => {
     const { data } = await axios.get(BASE_URL);
     return data;
   };
 
-  // const getRandRecipe = () => {
-  //   const query = useQuery('recipe', callApi);
-  //   console.log(query);
-  // };
-
-  // useEffect(() => {
-  //   getRandRecipe();
-  // }, []);
-
-  const query = useQuery('recipe', callApi, {
-    refetchOnWindowFocus: false,
+  const { isLoading, isError, data, error } = useQuery('recipe', getRandMeal, {
+    refetchOnWindowFocus: false, // react-query는 사용자가 사용하는 윈도우가 다른 곳을 갔다가 다시 화면으로 돌아오면 이 함수를 재실행합니다. 그 재실행 여부 옵션 입니다.
+    retry: 0, // 실패시 재호출 몇번 할지
+    onSuccess: (data) => {
+      // 성공시 호출
+      console.log(data);
+    },
+    onError: (e) => {
+      // 실패시 호출 (401, 404 같은 error가 아니라 정말 api 호출이 실패한 경우만 호출됩니다.)
+      // 강제로 에러 발생시키려면 api단에서 throw Error 날립니다. (참조: https://react-query.tanstack.com/guides/query-functions#usage-with-fetch-and-other-clients-that-do-not-throw-by-default)
+      console.log(e.message);
+    },
   });
-  console.log(query.data);
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
+  console.log(data.meals[0]);
 
   return (
     <div className="App">
       <h1>react-query example</h1>
+      <span>{data.meals[0].strMeal}</span>
     </div>
   );
 }
